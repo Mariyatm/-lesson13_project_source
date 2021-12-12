2b1e90a96516809f14b6fd3cd9eeaa09756dfa23
mariazks <mariazks@yandex-team.ru>
2021-12-12 21:23:45 +0300

    update functions

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template, send_from_directory, jsonify
 from pathlib import Path
 import os, json
-from functions import is_tag, get_tag
+from functions import *
 
 POST_PATH = "posts.json"
 UPLOAD_FOLDER = os.path.join("uploads", "images")
@@ -11,27 +11,18 @@ app.config['JSON_AS_ASCII'] = False
 
 @app.route("/")
 def page_index():
-    with open(POST_PATH, "r") as f:
-        posts = json.load(f)
-    tags = set()
-    for post in posts:
-        for word in post["content"].split():
-            if is_tag(word):
-                tags.add(get_tag(word))
+    posts = read_json(POST_PATH)
+    tags = get_all_tags(posts)
     return render_template("index.html", tags=tags)
 
 
 @app.route("/tag")
 def page_tag():
     tag = request.args.get("tag")
-    with open(POST_PATH, "r") as f:
-        posts = json.load(f)
-    tag_posts = []
-    for post in posts:
-        if "#"+tag in post["content"]:
-            tag_posts.append(post)
-    print(tag_posts)
+    posts = read_json(POST_PATH)
+    tag_posts = get_posts_by_tags(tag, posts)
     return render_template("post_by_tag.html", tag=tag, posts=tag_posts)
+
 
 @app.route("/post", methods=["GET", "POST"])
 def page_post_create():
@@ -46,11 +37,7 @@ def page_post_create():
         content = request.form.get("content")
 
         pic_path = os.path.join(UPLOAD_FOLDER, file.filename)
-        with open(POST_PATH, "r") as f:
-            posts = json.load(f)
-        with open(POST_PATH, "w") as f:
-            posts.append({"pic": pic_path, "content": content})
-            json.dump(posts, f, ensure_ascii=False, indent=4)
+        add_post(POST_PATH, {"pic": pic_path, "content": content})
 
         return render_template("post_uploaded.html", path=pic_path, content=content)
 
--- functions.py
+++ functions.py
@@ -1,6 +1,39 @@
+import json
+
+
 def is_tag(word):
     return word.startswith("#")
 
 
 def get_tag(word):
     return word.replace("#","").replace("!","")
+
+
+def read_json(path):
+    with open(path, "r") as f:
+        data = json.load(f)
+    return data
+
+
+def get_all_tags(posts):
+    tags = set()
+    for post in posts:
+        for word in post["content"].split():
+            if is_tag(word):
+                tags.add(get_tag(word))
+    return tags
+
+
+def get_posts_by_tags(tag, posts):
+    tag_posts = []
+    for post in posts:
+        if "#" + tag in post["content"]:
+            tag_posts.append(post)
+    return  tag_posts
+
+
+def add_post(path, post):
+    posts = read_json(path)
+    with open(path, "w") as f:
+        posts.append(post)
+        json.dump(posts, f, ensure_ascii=False, indent=4)
